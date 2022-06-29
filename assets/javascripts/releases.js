33a2ed430058d526ca3ddc7119385439b63f951f
Paulie14 <pavelexner@gmail.com>
2022-06-29 15:27:50 +0200

    Show added 2-0-0 version.

--- assets/javascripts/releases.js
+++ assets/javascripts/releases.js
@@ -1,7 +1,7 @@
 (function(window) {
   window.geomop = {};
 
-  window.geomop.defaultVersion  = "1.0.0";
+  window.geomop.defaultVersion  = "2.0.0";
   window.geomop.packageRoot     = 'http://geomop.nti.tul.cz/packages';
   window.geomop.releaseList     = [
       {
@@ -12,6 +12,11 @@
       {
         version:  "1.1.0",
         display:  "release 1.1.0",
+        links:    ['download', 'features', 'changes', 'manual', 'readme'],
+      },
+      {
+        version:  "2.0.0",
+        display:  "release 2.0.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
       },
   ];
