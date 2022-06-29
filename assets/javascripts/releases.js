351240828e8ce965f183bb742ece5fe60eaeaf49
Paulie14 <pavelexner@gmail.com>
2022-06-29 15:38:39 +0200

    Change order of versions displayed.

--- assets/javascripts/releases.js
+++ assets/javascripts/releases.js
@@ -5,8 +5,8 @@
   window.geomop.packageRoot     = 'http://geomop.nti.tul.cz/packages';
   window.geomop.releaseList     = [
       {
-        version:  "1.0.0",
-        display:  "release 1.0.0",
+        version:  "2.0.0",
+        display:  "release 2.0.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
       },
       {
@@ -15,8 +15,8 @@
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
       },
       {
-        version:  "2.0.0",
-        display:  "release 2.0.0",
+        version:  "1.0.0",
+        display:  "release 1.0.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
       },
   ];
