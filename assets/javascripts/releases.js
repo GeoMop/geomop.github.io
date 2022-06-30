6bd9e0b184f298569a0a0a69268af0f96b23c9cc
Jan Brezina <jan.brezina@tul.cz>
2022-06-30 10:31:17 +0200

    Try to differentiate documentation link

--- assets/javascripts/releases.js
+++ assets/javascripts/releases.js
@@ -8,16 +8,20 @@
         version:  "2.0.0",
         display:  "release 2.0.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
+        documentation_relative: html/index.html,
+        // link to documentation file (PDF or HTML) relative to geomop.nti.tul.cz/packages/{version}/
       },
       {
         version:  "1.1.0",
         display:  "release 1.1.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
+        documentation_relative: "Geomop 1.0.1 reference guide.pdf"
       },
       {
         version:  "1.0.0",
         display:  "release 1.0.0",
         links:    ['download', 'features', 'changes', 'manual', 'readme'],
+        documentation_relative: "Geomop 1.0.0 reference guide.pdf"
       },
   ];
 })(window);
